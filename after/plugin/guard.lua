c7d0fded5f4b2d4d7cd17fd2fc7ca1d8819cee1f
jdtoombs <jdtoombs@gmail.com>
2024-02-01 14:27:55 -0800

    rebind

--- after/plugin/guard.lua
+++ after/plugin/guard.lua
@@ -7,5 +7,5 @@ require('guard').setup({
     -- the only options for the setup function
     fmt_on_save = true,
     -- Use lsp if no formatter was defined for this filetype
-    lsp_as_default_formatter = false,
+    lsp_as_default_formatter = true,
 })
