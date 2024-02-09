97c92bb6ae8ba98d6a93b685037b731bbb54a55c
jdtoombs <jdtoombs@gmail.com>
2024-02-09 12:07:21 -0800

    add colorizer for styling files

--- lua/jdtoombs/packer.lua
+++ lua/jdtoombs/packer.lua
@@ -2,64 +2,64 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require('packer').startup(function(use)
-  use {'wbthomason/packer.nvim'}
+	use { 'wbthomason/packer.nvim' }
 
-  use 'nvim-tree/nvim-web-devicons'
+	use 'nvim-tree/nvim-web-devicons'
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
-    requires = {'nvim-lua/plenary.nvim'}
-  }
+	use {
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.4',
+		requires = { 'nvim-lua/plenary.nvim' }
+	}
 
-  use {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {}
-  }
+	use {
+		'folke/tokyonight.nvim',
+		lazy = false,
+		priority = 1000,
+		opts = {}
+	}
 
-  use {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    requires = {'nvim-lua/plenary.nvim'}
-  }
+	use {
+		'ThePrimeagen/harpoon',
+		branch = 'harpoon2',
+		requires = { 'nvim-lua/plenary.nvim' }
+	}
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
 
-  use 'tpope/vim-fugitive'
+	use 'tpope/vim-fugitive'
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    requires = {
-      -- LSP Support
-      'neovim/nvim-lspconfig',
-      -- Autocompletion
-      'hrsh7th/nvim-cmp',
-      'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip'
-    }
-  }
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			-- LSP Support
+			'neovim/nvim-lspconfig',
+			-- Autocompletion
+			'hrsh7th/nvim-cmp',
+			'hrsh7th/cmp-nvim-lsp',
+			'L3MON4D3/LuaSnip'
+		}
+	}
 
-  use {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'neovim/nvim-lspconfig'
-  }
+	use {
+		'williamboman/mason.nvim',
+		'williamboman/mason-lspconfig.nvim',
+		'neovim/nvim-lspconfig'
+	}
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'nvim-tree/nvim-web-devicons', opt = true}
-  }
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
 
-  use {'nvimdev/guard-collection'}
-  use {'nvimdev/guard.nvim'}
-  use {'terrortylor/nvim-comment'}
-  use {'github/copilot.vim'}
-  use {'windwp/nvim-ts-autotag'}
+	use { 'nvimdev/guard-collection' }
+	use { 'nvimdev/guard.nvim' }
+	use { 'terrortylor/nvim-comment' }
+	use { 'github/copilot.vim' }
+	use { 'windwp/nvim-ts-autotag' }
+	use { 'NvChad/nvim-colorizer.lua' }
 end)
-
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -129,6 +129,11 @@ _G.packer_plugins = {
     path = "/home/jdtoombs/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-colorizer.lua"] = {
+    loaded = true,
+    path = "/home/jdtoombs/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    url = "https://github.com/NvChad/nvim-colorizer.lua"
+  },
   ["nvim-comment"] = {
     loaded = true,
     path = "/home/jdtoombs/.local/share/nvim/site/pack/packer/start/nvim-comment",
