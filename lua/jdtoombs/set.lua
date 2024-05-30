60631cf206c93fe9c7b25caf2fae07dbd48f3cb1
jdtoombs <jdtoombs@gmail.com>
2024-05-30 14:01:41 -0700

    live grep & highlight line

--- lua/jdtoombs/set.lua
+++ lua/jdtoombs/set.lua
@@ -1,7 +1,10 @@
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight]]
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.api.nvim_set_hl(0, "@comment.doc", { fg = "#FF0000" })
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -10,3 +13,4 @@ vim.opt.shiftwidth = 4
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+vim.opt.cursorline = true
