d215990d2fc765a907fef3664a54426de281220c
jdtoombs <jdtoombs@gmail.com>
2024-11-11 14:48:54 -0800

    fix depr warning

--- after/plugin/guard.lua
+++ after/plugin/guard.lua
@@ -2,10 +2,12 @@ local ft = require('guard.filetype')
 
 -- Assuming you have guard-collection
 ft('typescript,javascript,typescriptreact'):fmt('prettier')
--- Call setup() LAST!
-require('guard').setup({
-    -- the only options for the setup function
-    fmt_on_save = true,
-    -- Use lsp if no formatter was defined for this filetype
-    lsp_as_default_formatter = true,
-})
+
+vim.g.guard_config = {
+	-- format on write to buffer
+	fmt_on_save = true,
+	-- use lsp if no formatter was defined for this filetype
+	lsp_as_default_formatter = false,
+	-- whether or not to save the buffer after formatting
+	save_on_fmt = true,
+}
