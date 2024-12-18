b91bde9be4cf45e6eed357ff6e1c4bf3cdaab52c
jdtoombs <jdtoombs@gmail.com>
2024-12-17 19:08:23 -0800

    telescope enhancements

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,23 @@
 return {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = { 'nvim-lua/plenary.nvim', { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
 	config = function()
+		require('telescope').setup({
+			extensions = {
+				fzf = {},
+			},
+		})
+		require('telescope').load_extension('fzf')
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>ps', function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") });
-		end)
+		vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>nc', function()
+			builtin.find_files({
+				prompt_title = 'Neovim Config',
+				cwd = vim.fn.stdpath('config'),
+			})
+		end, {})
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 	end,
 }
